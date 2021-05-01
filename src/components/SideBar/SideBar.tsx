6e52f545240634a80aec321444d5296504beb6e5
frankie0917 <frankie0917@foxmail.com>
2021-05-01 22:36:34 +0800

    added logo

--- src/components/SideBar/SideBar.tsx
+++ src/components/SideBar/SideBar.tsx
@@ -5,12 +5,16 @@ import { useFlowStore } from '../../store';
 import { parseNumber } from '../../utils/parseNumber';
 import { DebouncedInput as Input } from '../Form/DebounceInput';
 import { Disclosure } from './Disclosure/Disclosure';
+import { ReactComponent as Logo } from '../../static/Logo.svg';
 
 export const SideBar = observer(() => {
   const FlowStore = useFlowStore();
 
   return (
     <div className="SideBar bg-white p-2 shadow-lg max-w-xs w-80">
+      <div className="mb-5">
+        <Logo />
+      </div>
       <Disclosure title="Background">
         <Input
           label="Dot size"
